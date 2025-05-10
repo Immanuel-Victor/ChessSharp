474436eb4253ebcd41b1ae28c258fb0d26857273
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2025-05-10 15:58:10 -0300

    test: Adiciona testes de impressão de tabuleiro na tela

---
+++ ChessSharp.UnitTests/TelaTests.cs
@@ -0,0 +1,38 @@
+﻿using ChessSharp.Tabuleiro;
+using FluentAssertions;
+
+namespace ChessSharp.UnitTests;
+
+public class TelaTests
+{
+    [Theory]
+    [InlineData(8,8)]
+    public void Tela_ImprimirTabuleiro_ShowEmptyBoard(int linhas, int colunas)
+    {
+        //Arrange
+        var tab = new TabuleiroJogo(linhas, colunas);
+
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+        
+        //Act
+        
+        Tela.ImprimirTabuleiro(tab);
+        
+        using var sw2 = new StringWriter();
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                sw2.Write("- ");
+            }
+            sw2.WriteLine();
+        }
+        
+        //Assert
+        
+        var expectedOutput = sw2.ToString();
+
+        sw.ToString().Normalize().Should().Contain(expectedOutput.Normalize());
+    }
+}
