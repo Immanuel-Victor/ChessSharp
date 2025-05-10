9f01f9c8da4f28d46ceacbae0cf0f682b4874239
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2025-05-10 14:32:51 -0300

    feat: criando a classe Posicao

---
+++ ChessSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tabuleiro;
+
+namespace ChessSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Posicao p = new Posicao(3, 4);
+        
+        Console.WriteLine($"Posição: {p}");
+    }
+}
