2dc20e05b86aa08eda0f6fa84b32eee050585edc
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2025-05-10 20:33:27 -0300

    test: Adiciona testes de ToString para eventual impressão do tabuleiro

---
+++ ChessSharp.UnitTests/Xadrez/ReiTests.cs
@@ -0,0 +1,28 @@
+﻿using ChessSharp.Tabuleiro;
+using ChessSharp.Xadrez;
+using FluentAssertions;
+
+namespace ChessSharp.UnitTests.Xadrez;
+
+public class ReiTests
+{
+    private readonly TabuleiroJogo tabuleiroJogo;
+    
+    public ReiTests()
+    {
+        tabuleiroJogo = new TabuleiroJogo(8,8);
+    }
+    [Fact]
+    public void Rei_ToString_ShouldReturnR()
+    {
+        //Arrange
+        var rei = new Rei(tabuleiroJogo, Cor.Preta);
+        
+        //Act
+
+        var expected = rei.ToString();
+        
+        //Assert
+        expected.Should().Be("R");
+    }
+}
---
+++ ChessSharp.UnitTests/Xadrez/TorreTests.cs
@@ -0,0 +1,29 @@
+﻿using ChessSharp.Tabuleiro;
+using ChessSharp.Xadrez;
+using FluentAssertions;
+
+namespace ChessSharp.UnitTests.Xadrez;
+
+public class TorreTests
+{
+    
+    private readonly TabuleiroJogo tabuleiroJogo;
+    
+    public TorreTests()
+    {
+        tabuleiroJogo = new TabuleiroJogo(8,8);
+    }
+    [Fact]
+    public void Rei_ToString_ShouldReturnR()
+    {
+        //Arrange
+        var rei = new Torre(tabuleiroJogo, Cor.Preta);
+        
+        //Act
+
+        var expected = rei.ToString();
+        
+        //Assert
+        expected.Should().Be("T");
+    }
+}
