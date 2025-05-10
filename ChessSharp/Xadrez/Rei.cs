2c01e06e94ea3f65d43a9aa2f3536c3fe8cdc27e
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2025-05-10 20:32:43 -0300

    feat: Adiciona primeiros tipos de peças na camada do jogo de xadrez

---
+++ ChessSharp/Xadrez/Rei.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessSharp.Xadrez;
+using Tabuleiro;
+
+public class Rei : Peca
+{
+    public Rei(TabuleiroJogo tabuleiroJogo, Cor cor) : base(tabuleiroJogo, cor)
+    {
+    }
+
+    public override string ToString()
+    {
+        return "R";
+    }
+}
---
+++ ChessSharp/Xadrez/Torre.cs
@@ -0,0 +1,15 @@
+﻿using ChessSharp.Tabuleiro;
+
+namespace ChessSharp.Xadrez;
+
+public class Torre : Peca
+{
+    public Torre(TabuleiroJogo tabuleiroJogo, Cor cor) : base(tabuleiroJogo, cor)
+    {
+    }
+
+    public override string ToString()
+    {
+        return "T";
+    }
+}
